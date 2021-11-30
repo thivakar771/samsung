73a192d8cba1a9717b838861aa046f6ec9c3126f
thivakar771 <thivakar771@gmail.com>
2021-11-30 18:47:33 +0530

    done

---
+++ src/org/phone/ExternalDetails.java
@@ -0,0 +1,7 @@
+package org.phone;
+
+public class ExternalDetails {
+public void size() {
+System.out.println("size-12gb");
+}
+}
---
+++ src/org/phone/InternalStorage.java
@@ -0,0 +1,17 @@
+package org.phone;
+
+public class InternalStorage {
+private void processorName() {
+System.out.println("processor name-snapdrogan");
+}
+private void ramSize() {
+System.out.println("ramsize-4gp");
+}
+public static void main(String[] args) {
+	InternalStorage t = new InternalStorage();
+	t.processorName();
+	t.ramSize();
+	ExternalDetails y = new ExternalDetails();
+	y.size();
+}
+}
